6185faaa5174a9db644ce4e8e9a44a18c4ca4eb6
Dominik <44637946+dominikzorgnotti@users.noreply.github.com>
2019-12-22 17:25:52 +0100

    Create kubectl-lsdeploy.py
    Initial commit of the code

---
+++ kubectl-lsdeploy.py
@@ -0,0 +1,61 @@
+import kubernetes
+from prettytable import PrettyTable  # pip3 install PTable
+
+"""
+Author: Dominik Zorgnotti (@dominikzorgnotti)
+Version: 1.0
+Summary: Print aggregated information about the dependencies of deployments and pods
+"""
+
+""" Import modules:
+kubernetes -> Kubernetes Python client libraries: get all the required information
+prettytable -> and pretty table to create nice output"""
+
+
+""" Create an empty list that holds our dicts with the information """
+Podlist = list()
+
+# Configs can be set in Configuration class directly or using helper utility
+kubernetes.config.load_kube_config()
+
+# Get all the information from the API
+GetK8sConfiguration = kubernetes.client.Configuration()
+GetV1ApiApps = kubernetes.client.AppsV1Api(
+    kubernetes.client.ApiClient(GetK8sConfiguration))
+GetV1ApiCore = kubernetes.client.CoreV1Api(
+    kubernetes.client.ApiClient(GetK8sConfiguration))
+
+# Use helper objects to store the individual information
+# Replicaset provides info about the ReplicasetName, Deployment and NameSpace
+GetAllReplicaSets = GetV1ApiApps.list_replica_set_for_all_namespaces(
+    watch=False)
+GetAllPods = GetV1ApiCore.list_pod_for_all_namespaces(watch=False)
+
+""" Iterate through all pods and store the info about
+- the name of the pod
+- NameSpace
+- Replicaset 
+- and Node"""
+for MyPodItems in GetAllPods.items:
+    if (MyPodItems.metadata.owner_references) and (MyPodItems.metadata.owner_references[0].kind == "ReplicaSet"):
+        MyPodReplicaSet = MyPodItems.metadata.owner_references[0].name
+
+    Podlist.append({"PodName": MyPodItems.metadata.name, "PodNameSpace": MyPodItems.metadata.namespace,
+                    "PodReplicaSet": MyPodReplicaSet, "PodHostingNode": MyPodItems.status.host_ip})
+
+for MyReplicaItems in GetAllReplicaSets.items:
+    for MyPod in Podlist:
+        if MyPod["PodReplicaSet"] == MyReplicaItems.metadata.name:
+            MyPod.update(
+                {"PodDeployment": MyReplicaItems.metadata.owner_references[0].name})
+
+
+
+"""Print table based on the provided list with deployment information"""
+OutputTable = PrettyTable()
+OutputTable.field_names = ["Namespace",
+                               "Deployment", "ReplicaSet", "Pod", "Hosting node"]
+for MyPodInfo in Podlist:
+    OutputTable.add_row([MyPodInfo["PodNameSpace"], MyPodInfo["PodDeployment"],
+                         MyPodInfo["PodReplicaSet"], MyPodInfo["PodName"], MyPodInfo["PodHostingNode"]])
+print(OutputTable)
