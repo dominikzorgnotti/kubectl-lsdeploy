6ff13dbd1d9062c69d54cae3f9dc6e7c9dbab24b
dominikzorgnotti <dominik@why-did-it.fail>
2019-12-29 00:31:43 +0100

    version 1.1 baseline

--- kubectl-lsdeploy.py
+++ kubectl-lsdeploy.py
@@ -1,52 +1,90 @@
 #!/usr/bin/env python3
 import kubernetes
 from prettytable import PrettyTable
+import argparse
+import json
+import pyyaml
 
 """
 Author: Dominik Zorgnotti (@dominikzorgnotti)
-Version: 1.0
+Version: 1.1
 Summary: Print aggregated information about the dependencies of deployments and pods
 
 Import modules:
 kubernetes -> Kubernetes Python client libraries: get all the required information
-prettytable -> and pretty table to create nice output"""
+prettytable -> pretty table to create nice output
+argparse -> read arguments from the command line
+json, pyyaml -> output format 
+"""
 
-""" Create an empty list that holds our dicts with the information """
-Podlist = list()
+""" Prepare to hold our command line arguments in the variable args """
+parser = argparse.ArgumentParser(description='Command line arguments')
+parser.add_argument("-o", "--output", 
+                    choices=["table", "json", "yaml"],
+                    default="table", type=str, help="Output format: table (default), json or yaml")
 
-""" Configs can be set in Configuration class directly or using helper utility """
-kubernetes.config.load_kube_config()
-GetK8sConfiguration = kubernetes.client.Configuration()
-GetV1ApiApps = kubernetes.client.AppsV1Api(
-    kubernetes.client.ApiClient(GetK8sConfiguration))
-GetV1ApiCore = kubernetes.client.CoreV1Api(
-    kubernetes.client.ApiClient(GetK8sConfiguration))
+args = parser.parse_args()
 
-""" From Replicasets and Pods we can gather all the info we need """
-GetAllReplicaSets = GetV1ApiApps.list_replica_set_for_all_namespaces(
-    watch=False)
-GetAllPods = GetV1ApiCore.list_pod_for_all_namespaces(watch=False)
+def QueryK8sAPI():
+    """ Configs can be set in Configuration class directly or using helper utility """
+    kubernetes.config.load_kube_config()
+    GetK8sConfiguration = kubernetes.client.Configuration()
+    GetV1ApiApps = kubernetes.client.AppsV1Api(kubernetes.client.ApiClient(GetK8sConfiguration))
+    GetV1ApiCore = kubernetes.client.CoreV1Api(kubernetes.client.ApiClient(GetK8sConfiguration))
+    """ From Replicasets and Pods we can gather all the info we need """
+    GetAllReplicaSets = GetV1ApiApps.list_replica_set_for_all_namespaces(watch=False)
+    GetAllPods = GetV1ApiCore.list_pod_for_all_namespaces(watch=False)
 
-""" Iterate through all pods and store the info about the name of the pod, NameSpace, Replicaset and Node"""
-for MyPodItems in GetAllPods.items:
-    if (MyPodItems.metadata.owner_references) and (MyPodItems.metadata.owner_references[0].kind == "ReplicaSet"):
-        MyPodReplicaSet = MyPodItems.metadata.owner_references[0].name
+def GetAllInfo():
+    """ Iterate through all pods and store the info about the name of the pod, NameSpace, Replicaset and Node"""
+    for MyPodItems in GetAllPods.items:
+        if (MyPodItems.metadata.owner_references) and (MyPodItems.metadata.owner_references[0].kind == "ReplicaSet"):
+            MyPodReplicaSet = MyPodItems.metadata.owner_references[0].name
+    
+        PodList.append({"PodName": MyPodItems.metadata.name, "PodNameSpace": MyPodItems.metadata.namespace,
+                        "PodReplicaSet": MyPodReplicaSet, "PodHostingNode": MyPodItems.status.host_ip})
+    
+    """ Iterate through all Replicasets and match these to the pods. From the replicaset we supply the deployment info to the dict """
+    for MyReplicaItems in GetAllReplicaSets.items:
+        for MyPod in PodList:
+            if MyPod["PodReplicaSet"] == MyReplicaItems.metadata.name:
+                MyPod.update(
+                    {"PodDeployment": MyReplicaItems.metadata.owner_references[0].name})
 
-    Podlist.append({"PodName": MyPodItems.metadata.name, "PodNameSpace": MyPodItems.metadata.namespace,
-                    "PodReplicaSet": MyPodReplicaSet, "PodHostingNode": MyPodItems.status.host_ip})
-
-""" Iterate through all Replicasets and match these to the pods. From the replicaset we supply the deployment info to the dict """
-for MyReplicaItems in GetAllReplicaSets.items:
-    for MyPod in Podlist:
-        if MyPod["PodReplicaSet"] == MyReplicaItems.metadata.name:
-            MyPod.update(
-                {"PodDeployment": MyReplicaItems.metadata.owner_references[0].name})
-
-"""Print table based on the provided list with deployment information"""
-OutputTable = PrettyTable()
-OutputTable.field_names = ["Namespace",
+def OutputAsTable (K8sApiInput):
+    """Print table based on the provided list with deployment information"""
+    OutputTable = PrettyTable()
+    OutputTable.field_names = ["Namespace",
                                "Deployment", "ReplicaSet", "Pod", "Hosting node"]
-for MyPodInfo in Podlist:
-    OutputTable.add_row([MyPodInfo["PodNameSpace"], MyPodInfo["PodDeployment"],
+    for MyPodInfo in K8sApiInput:
+        OutputTable.add_row([MyPodInfo["PodNameSpace"], MyPodInfo["PodDeployment"],
                          MyPodInfo["PodReplicaSet"], MyPodInfo["PodName"], MyPodInfo["PodHostingNode"]])
-print(OutputTable)
+    print(OutputTable)
+
+def OutputAsYaml (K8sApiInput):
+    """return yaml based on the provided list with deployment information"""
+    print("Return as YAML not implemented yet")
+
+def OutputAsJson (K8sApiInput):
+    """return json based on the provided list with deployment information"""
+    print("Return as JSON not implemented yet")
+
+def main():
+    """ Create an empty list that holds our dicts with the information """
+    PodList = list()
+    """ get infos from kubernetes """
+    QueryK8sAPI()
+    """ x """
+    GetAllInfo()
+    if args.output == "table":
+        OutputAsTable(PodList)
+    elif args.output == "yaml":
+        OutputAsTable(PodList)
+    elif args.output == "json":
+        OutputAsTable(PodList)
+
+if __name__ == "main":
+    main()
+
+
+## return values from functions
